de08e55e996d99f4bd151f7de7a3e7d984aec22a
Andres Fernandez <andres94.dev@gmail.com>
2025-05-02 14:55:14 +0200

    moludos con mjs

---
+++ mjs/index.mjs
@@ -0,0 +1,8 @@
+// .mjs para utilizar ES Modules
+// LA forma mas nueva y con mas ventajas para exportar modulos
+
+import {sum, sub, mult} from './sum.mjs'
+
+console.log(sum(8,7))
+console.log(sub(8,7))
+console.log(mult(8,7))
