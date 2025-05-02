bcb578fcaf777b6c00beb9d5908c1cbfc7b7e7b3
Andres Fernandez <andres94.dev@gmail.com>
2025-05-02 14:49:56 +0200

    metodo clasico de importación de moludos CommonJS

---
+++ cjs/index.js
@@ -0,0 +1,4 @@
+// Metodo clásico (Common JS) require module
+const {sum} = require('./sum')
+
+console.log(sum(2,4))
---
+++ cjs/sum.js
@@ -0,0 +1,8 @@
+function sum (a, b) {
+  return a + b
+}
+
+// Common js export
+module.exports = {
+  sum
+}
