bd634282e7978385eb2f968539ed4f864b2f9659
Andres Fernandez <andres94.dev@gmail.com>
2025-05-06 14:04:15 +0200

    Agregar soporte para eliminar películas y mejorar la gestión de CORS en la API

--- clase-03/app.js
+++ clase-03/app.js
@@ -1,11 +1,20 @@
 const express = require('express')
 const crypto = require('node:crypto')
+const cors = require('cors')
 const moviesJSON = require('./movies.json')
 const { validateMovie, validateParcialMovie } = require('./schemas/movies')
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 app.disable('x-powered-by')
+
+// const ACCEPTED_ORIGINS = [
+//   'http://localhost:8080',
+//   'http://localhost:1234',
+//   'http://localhost:3000',
+//   'http://movies.com',
+// ]
 
 // URL base -------------
 app.get('/', (req, res) => {
@@ -14,6 +23,8 @@ app.get('/', (req, res) => {
 
 // Obtener todas las peliculas -----------
 app.get('/movies', (req, res) => {
+  // const origin = req.header('origin')
+  // if(ACCEPTED_ORIGINS.includes(origin) || !origin) res.header('Access-Control-Allow-Origin', origin)
   const { genre } = req.query
   if (genre) {
     const filteredMovies = moviesJSON.filter(movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase()))
@@ -65,6 +76,29 @@ app.patch('/movies/:id', (req, res) => {
   moviesJSON[movieIndex] = updateMovie
   return res.json(updateMovie)
 })
+
+// Eliminar una pelicula -----------
+app.delete('/movies/:id', (req, res) => {
+  // const origin = req.header('origin')
+  // if(ACCEPTED_ORIGINS.includes(origin) || !origin) res.header('Access-Control-Allow-Origin', origin)
+
+  const { id } = req.params
+  const movieIndex = moviesJSON.findIndex(movie => movie.id === id)
+  if (movieIndex < 0) return res.status(404).json({message: "Movie id not found"})
+
+  moviesJSON.splice(movieIndex, 1)
+  res.json({message: "Movie deleted"})
+})
+
+//Añade el CORS en OPTION
+// app.options('/movies/:id', (req, res) => {
+//   const origin = req.header('origin')
+//   if(ACCEPTED_ORIGINS.includes(origin) || !origin) {
+//     res.header('Access-Control-Allow-Origin', origin)
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
+//   }
+//   res.send(200)
+// })
 
 // ------------------------------------INICIO DE API ---------------------------------------------
 const PORT = process.env.PORT ?? 1234
