232af07a47de4e9539fafa6db19c5a4b8ab5bc82
Andres Fernandez <andres94.dev@gmail.com>
2025-05-07 14:03:25 +0200

    implementando funcionalidad al metodo getAll (con generos) y getById

--- clase-05/models/mysql/movie.js
+++ clase-05/models/mysql/movie.js
@@ -12,11 +12,40 @@ const connection = await mysql.createConnection(config)
 
 export class MovieModel {
   static async getAll ({ genre }) {
-    const [movies] = await connection.query('select title, year, director, duration, poster, rate, bin_to_uuid(id) id from movies;')
-    console.log(movies)
+    if (genre) {
+      const lowerCaseGenre = genre.toLowerCase()
+
+      const [genres] = await connection.query(
+        'SELECT id, name FROM genres WHERE LOWER(name) = ?;',
+        [lowerCaseGenre]
+      )
+
+      if (genres.length === 0) return []
+
+      const [{ id }] = genres
+
+      const [moviesId] = await connection.query('SELECT BIN_TO_UUID(movie_id) id FROM movie_genres WHERE genre_id = ?;', [id])
+
+      const moviePromises = moviesId.map(({ id }) =>
+        connection.query('SELECT title, year, director, duration, poster, rate, BIN_TO_UUID(id) id FROM movies WHERE id = UUID_TO_BIN(?);', [id])
+          .then(([movie]) => movie[0])
+      )
+
+      const movies = await Promise.all(moviePromises)
+      return movies
+    }
+
+    const [movies] = await connection.query('SELECT title, year, director, duration, poster, rate, BIN_TO_UUID(id) id FROM movies;')
+    return movies
   }
 
-  static async getById ({ id }) {}
+  static async getById ({ id }) {
+    const [movies] = await connection.query(
+      'SELECT title, year, director, duration, poster, rate, BIN_TO_UUID(id) id FROM movies WHERE id = UUID_TO_BIN(?);', [id]
+    )
+    if (movies.length === 0) return null
+    return movies[0]
+  }
 
   static async create ({ input }) {}
 
