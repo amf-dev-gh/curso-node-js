036e8441874fd8ab0c42c58caf6d9bd7c6379284
Andres Fernandez <andres94.dev@gmail.com>
2025-05-07 12:06:46 +0200

    inicializar package.json y crear base de movieModel para mysql

---
+++ clase-05/models/mysql/movie.js
@@ -0,0 +1,11 @@
+export class MovieModel {
+  static async getAll ({ genre }) {}
+
+  static async getById ({ id }) {}
+
+  static async create ({ input }) {}
+
+  static async deleteById ({ id }) {}
+
+  static async update ({ id, input }) {}
+}
