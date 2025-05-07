64a5b9ca62431eba893dcbdaa7c2fab1ea7375b7
Andres Fernandez <andres94.dev@gmail.com>
2025-05-07 10:38:11 +0200

    Agregar modelo MovieModel para gestionar películas desde el sistema de archivos local

--- clase-04/controllers/movies.js
+++ clase-04/controllers/movies.js
@@ -1,4 +1,4 @@
-import { MovieModel } from '../models/movie.js'
+import { MovieModel } from '../models/local-file-system/movie.js'
 import { validateMovie, validateParcialMovie } from '../schemas/movies.js'
 
 export class MovieController {
--- clase-04/models/local-file-system/movie.js
+++ clase-04/models/local-file-system/movie.js
@@ -1,7 +1,7 @@
 import { randomUUID } from 'node:crypto'
 import { createRequire } from 'node:module'
 const require = createRequire(import.meta.url)
-const movies = require('../movies.json')
+const movies = require('../../movies.json')
 
 export class MovieModel {
   static async getAll ({ genre }) {
