4e7dc03ee708870990269d2715a4ac7168f1d9e4
Andres Fernandez <andres94.dev@gmail.com>
2025-05-08 10:51:06 +0200

    creando web socket en servidor

--- clase-06/server/index.js
+++ clase-06/server/index.js
@@ -1,13 +1,31 @@
 import express from 'express'
+import logger from 'morgan'
+
+import { Server } from 'socket.io'
+import { createServer } from 'node:http'
 
 const PORT = process.env.PORT ?? 3000
 
 const app = express()
+const server = createServer(app)
+const io = new Server(server)
 
-app.get('/', (req, res) => {
-  res.send('<h1>Esto es el chat</h1>')
+io.on('connection', (socket) => {
+  console.log('A user has connected!')
+  socket.on('disconnect', () => {
+    console.log('User disconnected')
+  })
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg)
+  })
 })
 
-app.listen(PORT, () => {
+app.use(logger('dev'))
+
+app.get('/', (req, res) => {
+  res.sendFile(process.cwd() + '/client/index.html')
+})
+
+server.listen(PORT, () => {
   console.log(`App running on http://localhost:${PORT}`)
 })
