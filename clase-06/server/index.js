943259451e21f4471510f80db362d6c333456d7e
Andres Fernandez <andres94.dev@gmail.com>
2025-05-08 12:36:12 +0200

    agregando conexion de base de datos creada en TURSO

--- clase-06/server/index.js
+++ clase-06/server/index.js
@@ -1,14 +1,22 @@
 import express from 'express'
 import logger from 'morgan'
-
 import { Server } from 'socket.io'
 import { createServer } from 'node:http'
+import dotenv from 'dotenv'
+import { createClient } from '@libsql/client'
 
 const PORT = process.env.PORT ?? 3000
 
 const app = express()
 const server = createServer(app)
-const io = new Server(server)
+const io = new Server(server, {
+  connectionStateRecovery: {}
+})
+
+const db = createClient({
+  url: 'libsql://elegant-huntara-andres94dev.aws-eu-west-1.turso.io',
+  authToken: process.env.DB_TOKEN
+})
 
 io.on('connection', (socket) => {
   console.log('A user has connected!')
