14dd714d351c3fc239830d9b3a9fb8a389a84bf9
Andres Fernandez <andres94.dev@gmail.com>
2025-05-10 21:20:14 +0200

    implementa autenticación con JWT y manejo de cookies; agrega middleware para verificar sesión y protege rutas

--- clase-07/index.js
+++ clase-07/index.js
@@ -1,13 +1,28 @@
 import express from 'express'
-import { PORT } from './config.js'
+import jwt from 'jsonwebtoken'
+import cookieParser from 'cookie-parser'
+import { JWT_EXPIRATION_TIME, JWT_SECRET_KEY, PORT } from './config.js'
 import { UserRepository } from './user-repository.js'
 import { validateUser } from './validations.js'
 
 const app = express()
 app.use(express.json())
+app.set('view engine', 'ejs')
+app.use(cookieParser())
+// El siguiente midelware agrega la información a la request y continua con las rutas.
+app.use((req, res, next) => {
+  const token = req.cookies.access_token
+  req.session = { user: null }
+  try {
+    const data = jwt.verify(token, JWT_SECRET_KEY)
+    req.session.user = data
+  } catch {}
+  next()
+})
 
 app.get('/', (req, res) => {
-  res.send('<h1>Inicio de app para autenticar!</h1>')
+  const { user } = req.session
+  res.render('index', user)
 })
 
 app.post('/login', async (req, res) => {
@@ -16,13 +31,26 @@ app.post('/login', async (req, res) => {
   try {
     const { username, password } = req.body
     const user = await UserRepository.login({ username, password })
-    res.send({ user })
+    const token = jwt.sign({ id: user._id, username: user.username }, JWT_SECRET_KEY, { expiresIn: JWT_EXPIRATION_TIME })
+    res
+      .cookie(
+        'access_token',
+        token,
+        {
+          httpOnly: true, // Solo se puede acceder a la cookie desde el servidor, no desde el cliente(js, navegador)
+          secure: process.env.NODE_ENV === 'production', // Solo se pùede acceder a la cookie en https
+          sameSite: 'strict', // Solo se pùede acceder a la cookie desde el mismo dominio
+          maxAge: 1000 * 60 * 60 // Tiempo de expiración de la cookie
+        })
+      .send({ user })
   } catch (error) {
     res.status(401).send(error.message)
   }
 })
 
-app.post('/logout', (req, res) => {})
+app.post('/logout', (req, res) => {
+  res.clearCookie('access_token').json({ message: 'Sesión cerrada correctamente' })
+})
 
 app.post('/register', async (req, res) => {
   const result = validateUser(req.body)
@@ -31,14 +59,24 @@ app.post('/register', async (req, res) => {
   try {
     const { username, password } = req.body
     const id = await UserRepository.create({ username, password })
-    res.send({ id })
+    const user = await UserRepository.login({ username, password })
+    const token = jwt.sign({ id, username }, JWT_SECRET_KEY, { expiresIn: JWT_EXPIRATION_TIME })
+    res.cookie('access_token', token,
+      {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'strict',
+        maxAge: 1000 * 60 * 60
+      }).send({ user })
   } catch (error) {
     res.status(400).json({ message: error.message })
   }
 })
 
 app.get('/protected', (req, res) => {
-  // res.send('<h1>Ruta protegida</h1>')
+  const { user } = req.session
+  if (!user) return res.status(403).send('Acceso no autorizado')
+  res.render('protected', user)
 })
 
 app.listen(PORT, () => {
