01ae54bc8f03fcb2bd124e55fe132fbf7738a129
Andres Fernandez <andres94.dev@gmail.com>
2025-05-10 21:19:56 +0200

    agrega JWT_SECRET_KEY y JWT_EXPIRATION_TIME a la configuración

--- clase-07/config.js
+++ clase-07/config.js
@@ -1,4 +1,6 @@
 export const {
   PORT = 4321,
-  SALT_ROUNDS = 10
+  SALT_ROUNDS = 10,
+  JWT_SECRET_KEY = 'aqui-la-clave-srcreta_para_firmar?el?JTW-segura?=)',
+  JWT_EXPIRATION_TIME = '1h'
 } = process.env
